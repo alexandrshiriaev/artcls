c8849702dd7fd0d5d3639f50a3bc8dc9e19e3fb6
Alexandr Shiriaev <alexandrshiriaev7@gmail.com>
2024-09-24 23:10:33 +0300

    fix(auth): add all config fields to NextAuth initializer

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -21,6 +21,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     session: {
         strategy: 'jwt',
     },
+    ...authConfig,
     providers: [
         ...authConfig.providers,
         Credentials({
